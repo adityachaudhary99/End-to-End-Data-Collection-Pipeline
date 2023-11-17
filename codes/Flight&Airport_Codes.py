d9bf2781c72b0f619b8aa716de13a648720dc439
aditya <adityaachaudhary2003@gmail.com>
2023-11-17 23:19:05 +0530

    Extracted airport and flight data

---
+++ codes/Flight&Airport_Codes.py
@@ -0,0 +1,93 @@
+import configparser
+import requests
+import pandas as pd
+from datetime import datetime
+from pytz import timezone
+
+
+def airport_codes(cities, API_key):
+    icao_list = []
+
+    url = "https://flightradar24-com.p.rapidapi.com/airports/search"
+
+    ids_list = []
+    for city in cities:
+        querystring = {"q": city}
+
+        headers = {
+            "X-RapidAPI-Key": API_key,
+            "X-RapidAPI-Host": "flightradar24-com.p.rapidapi.com"
+        }
+
+        response = requests.get(url, headers=headers, params=querystring)
+        result = response.json()
+
+        for i in result['data']:
+            ids_list.append(i['id'])
+
+    # Different API call for airport codes
+    for id in ids_list:
+        url = "https://flightradar24-com.p.rapidapi.com/airports/detail"
+
+        querystring = {"airport_id": id}
+
+        headers = {
+            "X-RapidAPI-Key": API_key,
+            "X-RapidAPI-Host": "flightradar24-com.p.rapidapi.com"
+        }
+
+        response = requests.get(url, headers=headers, params=querystring)
+        result = response.json()
+
+        if result['data']['airport']['pluginData']['details']['code']['iata'] == id:
+            icao_list.append(result['data']['airport']['pluginData']['details']['code']['icao'])
+
+    return icao_list
+
+
+def flight_arrivals(icao_list, API_key):
+    # Initialize an empty list to store flight data
+    list_for_df = []
+
+    # Loop over each ICAO code in the input list
+    for icao in icao_list:
+        params = {
+            'access_key': API_key,
+            'arr_icao': icao
+        }
+
+        api_result = requests.get('http://api.aviationstack.com/v1/flights', params)
+
+        api_response = api_result.json()
+
+        for flight in api_response['data']:
+            flight_dict = {}
+
+            flight_dict['arrival_icao'] = icao
+            flight_dict['arrival_time_local'] = flight['arrival']['scheduled']
+            flight_dict['arrival_terminal'] = flight['arrival']['terminal']
+            flight_dict['departure_city'] = flight['departure']['timezone']
+            flight_dict['departure_icao'] = flight['departure']['icao']
+            flight_dict['departure_time_local'] = flight['departure']['scheduled']
+            flight_dict['airline'] = flight['airline']['name']
+            flight_dict['flight_number'] = flight['flight']['number']
+            flight_dict['data_retrieved_on'] = datetime.now().astimezone(timezone('Europe/Berlin')).date()
+
+            list_for_df.append(flight_dict)
+
+    # Convert the list of flight dictionaries to a DataFrame and return it
+    return pd.DataFrame(list_for_df)
+
+
+cities = ['Berlin', 'Hamburg', 'London']
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+airport_API_key = config['secret']['airport_API_key']
+flight_API_key = config['secret']['flight_API_key']
+
+airport_codes = airport_codes(cities, airport_API_key)
+icao_list = airport_codes
+
+print(icao_list)
+print(flight_arrivals(icao_list, flight_API_key))
